607fabd7e65003d909464bb9a231759fa4e43dd3
chhaya.yadav <chhaya.yadav@PC90-PC>
2024-05-20 12:29:21 +0530

    firstfg#

---
+++ SafetyClone project/Pages/adminScreen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SafetyClone_project.Pages
+{
+    public class adminScreenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
